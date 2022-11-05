17e6cff500e0041fdadff92432d034967cdf35a0
Pedro Mantovani <mantovani.pedro99@gmail.com>
2022-11-04 21:04:08 -0300

    clean fix

--- Server/Routes/AuthRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { loginUer, registerUser } from "../Controllers/AuthController.js";
-
-const router = express.Router();
-
-router.post("/register", registerUser);
-router.post("/login", loginUer);
-
-export default router;
